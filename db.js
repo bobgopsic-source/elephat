d0a8de9961f702e33b9b80d7ad5a925e84f4855a
bobgopsic-source <bobgopsic@gmail.com>
2025-10-13 11:01:19 +0800

    Delete db.js

--- db.js
+++
@@ -1,6 +0,0 @@
-
-const DB_NAME='vocabDBv10', STORE='wordbooks';
-function openDB(){return new Promise((res,rej)=>{const rq=indexedDB.open(DB_NAME,1); rq.onupgradeneeded=e=>{const db=e.target.result; if(!db.objectStoreNames.contains(STORE)) db.createObjectStore(STORE,{keyPath:'id'});}; rq.onsuccess=()=>res(rq.result); rq.onerror=()=>rej(rq.error);});}
-async function saveWordbook(book){const db=await openDB(); const tx=db.transaction(STORE,'readwrite'); tx.objectStore(STORE).put(book); return tx.complete;}
-async function getAllWordbooks(){const db=await openDB(); return new Promise(res=>{const r=db.transaction(STORE).objectStore(STORE).getAll(); r.onsuccess=()=>res(r.result||[]);});}
-async function getWordbook(id){const db=await openDB(); return new Promise(res=>{const r=db.transaction(STORE).objectStore(STORE).get(id); r.onsuccess=()=>res(r.result);});}
